8f9d8b9b95cf3bc0578357992b59feee54328884
Subham Choudhury <subham@localhost.localdomain>
2023-11-07 07:38:20 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import "./index.css";
+import {useState} from "react";
+
+
+export default function App(){
+  const [temp,setTemp] = useState(36);
+  const [clr,setClr] = useState("orange");
+  
+  function increaseTemp() {
+    let newTemp = temp+1;
+    if(newTemp>=40){
+      setClr("red");
+    }
+    else if(newTemp>34 & newTemp<40){
+      setClr("orange");
+    }
+    else {setClr("blue");}
+    setTemp(newTemp)
+  }
+  function decreaseTemp() {
+    let newTemp=temp-1;
+    if(newTemp>=40){
+      setClr("red");
+    }
+    else if(newTemp<40 && newTemp>34){
+      setClr("orange");
+    }
+    else{
+      setClr("blue");
+    }
+    setTemp(newTemp)
+  }
+  return(
+    <>
+      <div className ="container">
+        <div className ={`display-container , ${clr}`}>
+          <div className ="display">{temp}°C</div>
+        </div>
+        <div className ="btn-container">
+          <button onClick={increaseTemp}>+</button>
+          <button onClick={decreaseTemp}>-</button>
+        </div>
+      </div>
+    </>
+  );
+}
